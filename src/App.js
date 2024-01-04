d648a46b544d217ec827a94a4d3fda5dd6e083b6
sara-sh80 <60575647+sara-sh80@users.noreply.github.com>
2024-01-04 21:09:19 +0330

    update hw

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {Route, Switch} from "react-router-dom";
+import Page from "./Components/Page";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Switch>
+                    <Route path="/:page_id" component={Page}/>
+                    <Route path="/" component={Page}/>
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
